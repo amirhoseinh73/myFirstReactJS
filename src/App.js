7117c03ce6309deb7cc643fd6bf792120afe69a6
amirhosein hasani <amirhoseinh1373@gmail.com>
2022-06-01 17:32:25 +0430

    install prettier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        React.createElement( "h1", {}, "Adopt Me!" )
-    );
-}
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
+};
 
-ReactDOM.render( React.createElement( App ), document.getElementById( "root" ) );
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+    // ...[ 1, 2, 3, 4 ].map( i => React.createElement( "h2", {}, i ) ),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havenese",
+    }),
+    React.createElement(Pet, {
+      name: "jimi",
+      animal: "cat",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "bernard",
+      animal: "bird",
+      breed: "Wheaten Terrier",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
